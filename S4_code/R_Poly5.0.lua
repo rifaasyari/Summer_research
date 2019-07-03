2cac268a3fa4d8ad21f7e62054f78ca9c061bd0a
Shengnan Han <jeffery@Shengnans-iMac.local>
2019-07-03 09:41:57 -0700

    wkst

--- S4_code/R_Poly5.0.lua
+++ S4_code/R_Poly5.0.lua
@@ -70,7 +70,7 @@ end
 
 ------------------------------- Main starts here -------------------------------
 
-local file = 'data/DATA12_#40_gen.txt'
+local file = 'data/DATA14_#37_gen.txt'
 local data = lines_from(file)
 
 size = 40
