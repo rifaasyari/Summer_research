9bd6be614ee6ae925138a0418b3f78433afba147
Shengnan Han <jeffery@Shengnans-iMac.local>
2019-07-03 09:46:49 -0700

    data

--- S4_code/R_Poly5.0.lua
+++ S4_code/R_Poly5.0.lua
@@ -70,10 +70,10 @@ end
 
 ------------------------------- Main starts here -------------------------------
 
-local file = 'data/DATA14_#37_gen.txt'
+local file = 'data/DATA15_#37_gen.txt'
 local data = lines_from(file)
 
-size = 40
+size = 48
 count = 0
 --print(#data)
 for i = 1, #data, size do
