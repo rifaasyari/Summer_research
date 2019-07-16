dd0124c2aa8f712b0d3b4fb47ad2cef75c4a8027
Shengnan Han <jeffery@Shengnans-iMac.local>
2019-07-15 21:05:00 -0700

    data

--- S4_code/Verify.lua
+++ S4_code/Verify.lua
@@ -1,4 +1,28 @@
+------------------------------- Global Variables -------------------------------
+SYMMETRYDEG = math.pi / 180
+math.randomseed(os.time())
+SYMMETRY = {{1, 1}, {-1, 1}, {-1, -1}, {1, -1}}
+ANGLE = {0,9,18,27,36,45}
+LINE = 6
 --------------------------------- Functions ------------------------------------
+-- return a copy
+function table.clone(org)
+    return {table.unpack(org)}
+end
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+function lines_from(file)
+    if not file_exists(file) then return {} end
+    lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = tonumber(line)
+    end
+    return lines
+end
 -- generate the full shape
 function gen_full_shape(base, SYMMETRY, shape)
     for i = 1, #SYMMETRY, 1 do
@@ -17,13 +41,20 @@ function gen_full_shape(base, SYMMETRY, shape)
         end
     end
 end
+-- return one_8th of the shape
+function one_8th(length, ANGLE)
+    local base = {}
+    for i = 1, LINE, 1 do
+        x = length[i] * math.cos(ANGLE[i] * SYMMETRYDEG)
+        y = length[i] * math.sin(ANGLE[i] * SYMMETRYDEG)
+        table.insert(base, {x, y})
+    end
+    return table.clone(base)
+end
 
-function filter_duplicate(origin, gen)
+function filter_duplicate(origin)
     local result = {}
     local offset = 2
-    if gen == 0 then
-        offset = 0
-    end
     for i = 1, #origin - offset, 2 do
         if i > 2 and math.abs(origin[i - 2] - origin[i]) <= 0.00001 and
                      math.abs(origin[i + 1] - origin[i - 1]) <= 0.00001 then
@@ -39,11 +70,17 @@ end
 
 ------------------------------- Main starts here -------------------------------
 
-local file = 'data/DATA57_#35_gen.txt'
+local file = '../Prediction/2019_7_15_1_p.txt'
 local data = lines_from(file)
+--print(#data)
 
-base = {unpack(data, 1, 8}
-for i = LINE, 1, -1 do
+base = one_8th(data, ANGLE)
+
+--for i = 1, #base, 1 do
+--    print(base[i][1], base[i][2])
+--end
+
+for i = 6, 1, -1 do
     table.insert(base, {base[i][2], base[i][1]})
 end
 -- propagate through four quarters
@@ -54,15 +91,13 @@ gen_full_shape(base, SYMMETRY, shape)
 --end
 
 
-base = {unpack(data, 1, 8}
-print(#base)
-for i = #base, 1, -1 do
+for i = LINE, 1, -1 do
     table.insert(base, {base[i][2], base[i][1]})
 end
 -- propagate through four quarters
 shape = {}
 gen_full_shape(base, SYMMETRY, shape)
-shape2 = filter_duplicate(shape, activate[1])
+shape2 = filter_duplicate(shape)
 -- test shape
 --for qq = 1, #shape2, 2 do
 --    print(shape2[qq], shape2[qq + 1])
@@ -70,5 +105,5 @@ shape2 = filter_duplicate(shape, activate[1])
 --print(activate[1])
 --print(#shape2)
 for qq = 1, #shape2, 1 do
-   print(shape2[qq])
+  print(shape2[qq])
 end
